322a8d57b1fa2c9f0049241cc11b8b90b30168a0
Dinar <dinar-wp9@mail.ru>
2022-11-22 11:14:11 +0300

    Add project files.

---
+++ Controllers/ChooseController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace RobotWord.Controllers
+{
+    public class ChooseController
+    {
+        private readonly ITelegramBotClient _telegramclient;
+        public ChooseController(ITelegramBotClient telegramclient)
+        {
+            _telegramclient = telegramclient;
+        }
+        public async Task Handle(Message message, CancellationToken ct)
+        {
+            switch (message.Text)
+            {
+                case "/start":
+                    var choose = new List<InlineKeyboardButton[]>();
+                    choose.Add(new[]
+                    {
+                        InlineKeyboardButton.WithCallbackData($"Числа", $"sum"),
+                        InlineKeyboardButton.WithCallbackData($"Текст", $"char")
+                    }) ;
+                    await _telegramclient.SendTextMessageAsync(message.Chat.Id,
+                        $"Наш бот может посчитать сумму чисел или сумму символов в текстовом сообщении.</b>{Environment.NewLine}");
+
+                    break;
+                default:
+                    await _telegramclient.SendTextMessageAsync(message.Chat.Id, "Отправьте текст или числа.",cancellationToken: ct);
+                    break;
+            }
+        }
+    }
+}
---
+++ Controllers/InlineKeyboard.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace RobotWord.Controllers
+{
+    public class InlineKeyboard
+    {
+        private ITelegramBotClient _telegramClient;
+
+        public InlineKeyboard(ITelegramBotClient telegramClient)
+        {
+            _telegramClient = telegramClient;
+        }
+        public async Task Handle(CallbackQuery? callbackQuery, CancellationToken ct)
+        {
+            string mainTable = callbackQuery.Data switch
+            {
+                "sum" => "Сумматор",
+                "char" => "Текст",
+                _ => String.Empty
+
+            };
+            await _telegramClient.SendTextMessageAsync(callbackQuery.From.Id,
+                $"<b>Выбор - Можно поменять в главном меню.", cancellationToken: ct, parseMode: ParseMode.Html);
+
+        }
+        
+    }
+}
